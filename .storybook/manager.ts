4641711052b489f3929b81db2818a151c13e5528
Saksham Bindal <saksham.bindal2004@gmail.com>
2025-01-15 17:01:42 +0530

    show version in sidebar

---
+++ .storybook/manager.ts
@@ -0,0 +1,11 @@
+// .storybook/manager.ts
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+import packageJson from '../package.json';
+
+addons.setConfig({
+  theme: {
+    ...themes.light,
+    brandTitle: `React Apple Tree (v${packageJson.version})`,
+  },
+});
