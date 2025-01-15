c153f00583ac173cad113e1eb1a39e440a4714fe
Saksham Bindal <saksham.bindal2004@gmail.com>
2025-01-15 18:30:10 +0530

    add version dropdown

---
+++ .storybook/addons/VersionSwitcher.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useGlobals, useStorybookApi } from '@storybook/manager-api';
+
+interface Version {
+  id: string;
+  title: string;
+  url?: string;
+}
+
+const VERSIONS: Version[] = [{ id: 'current', title: 'Select Version' }];
+
+export default function VersionSwitcher() {
+  const [globals, updateGlobals] = useGlobals();
+  const api = useStorybookApi();
+
+  const handleVersionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedVersion = VERSIONS.find((v) => v.id === event.target.value);
+
+    if (selectedVersion?.url) {
+      window.location.href = `${window.location.origin}${selectedVersion.url}`;
+    } else {
+      updateGlobals({ version: selectedVersion?.id });
+    }
+  };
+
+  return (
+    <div className="version-switcher">
+      <select
+        value={globals.version || 'current'}
+        onChange={handleVersionChange}
+        style={{
+          margin: '0 15px',
+          padding: '5px 10px',
+          borderRadius: '4px',
+          border: '1px solid #ccc',
+        }}
+      >
+        {VERSIONS.map((version) => (
+          <option key={version.id} value={version.id}>
+            {version.title}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
