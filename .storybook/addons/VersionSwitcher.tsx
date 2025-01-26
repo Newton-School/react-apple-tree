aa63352145e9b42ead427a085d640efc36a48f73
Saksham Bindal <saksham.bindal2004@gmail.com>
2025-01-26 12:26:09 +0530

    Sync gh-pages with master branch (#91)
    Co-authored-by: Shyam Kumawat <72969645+shyamkumawat088@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .storybook/addons/VersionSwitcher.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { useGlobals } from '@storybook/manager-api';
+import versions from '../../versions.json';
+
+const VERSIONS: string[] = ['latest', ...versions.reverse()];
+
+export default function VersionSwitcher() {
+  const [globals, updateGlobals] = useGlobals();
+
+  const pathSegments = window.location.pathname.split('/');
+  const currentVersion = pathSegments[1];
+
+  useEffect(() => {
+    if (VERSIONS.some((version) => version === currentVersion)) {
+      updateGlobals({ version: currentVersion });
+    }
+  }, []);
+
+  const handleVersionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedVersion = VERSIONS.find((v) => v === event.target.value);
+    window.location.href = `${window.location.origin}/${selectedVersion}`;
+  };
+
+  return (
+    <div className="version-switcher">
+      <select
+        value={currentVersion || 'latest'}
+        onChange={handleVersionChange}
+        style={{
+          margin: '0 15px',
+          padding: '5px 10px',
+          borderRadius: '4px',
+          border: '1px solid #ccc',
+        }}
+      >
+        {VERSIONS.map((version) => (
+          <option key={version} value={version}>
+            {version}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,11 +1,21 @@
-import { addons } from '@storybook/addons';
+import { addons, types } from '@storybook/addons';
 import { themes } from '@storybook/theming';
+import packageJson from '../package.json';
+import React from 'react';
+import VersionSwitcher from './addons/VersionSwitcher';
 
-const version = process.env.LIBRARY_VERSION || 'Unknown Version';
+addons.register('storybook/version-switcher', () => {
+  addons.add('storybook/version-switcher', {
+    title: 'Versions',
+    type: types.TOOL,
+    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
+    render: () => React.createElement(VersionSwitcher),
+  });
+});
 
 addons.setConfig({
   theme: {
     ...themes.light,
-    brandTitle: `React Apple Tree (v${version})`,
+    brandTitle: `React Apple Tree (v${packageJson.version})`,
   },
 });
