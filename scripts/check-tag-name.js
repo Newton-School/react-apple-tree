68d44f83e27c740511afd1923976422f21f113df
Saksham Bindal <saksham.bindal2004@gmail.com>
2025-01-26 11:41:06 +0530

    add script to check tag name syntax

---
+++ scripts/check-tag-name.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const args = process.argv.slice(2);
+const tagName = args[0];
+const allowPre = args.includes('--allow-pre');
+
+if (!tagName) {
+  console.error('Error: Tag name is required.');
+  process.exit(1);
+}
+
+const versionRegex = /^v\d+\.\d+\.\d+$/;
+const preVersionRegex = /^v\d+\.\d+\.\d+-\d+$/;
+
+if (allowPre) {
+  if (versionRegex.test(tagName) || preVersionRegex.test(tagName)) {
+    console.log('Valid tag name.');
+  } else {
+    console.error('Invalid tag name.');
+    process.exit(1);
+  }
+} else if (versionRegex.test(tagName)) {
+  console.log('Valid tag name.');
+} else {
+  console.error('Invalid tag name.');
+  process.exit(1);
+}
